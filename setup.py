6cf816b037787d77d42e36e5b010f05b4fe1fa79
Michael Katz <michaelkatz@MacBook-404156.local>
2020-11-20 18:43:45 +0100

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+# from future.utils import iteritems
+from setuptools import setup
+
+setup(
+    name="LISAanalysistools",
+    # Random metadata. there's more you can supply
+    author="Michael Katz, Lorenzo Speri, Ollie Burke",
+    version="0.1",
+    packages=["LISAanalysistools.lisatools",],
+)
