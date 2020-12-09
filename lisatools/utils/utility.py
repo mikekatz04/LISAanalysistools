4c05087485ec2c17696cdd270f49710c74174b53
Michael Katz <michaelkatz@MacBook-404156.local>
2020-12-09 14:28:58 +0100

    utils added

---
+++ lisatools/utils/utility.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+from lisatools.sensitivity import get_sensitivity
+
+
+def generate_noise_fd(freqs, df, **sensitivity_kwargs):
+    norm = 0.5 * (1.0 / df) ** 0.5
+    psd = get_sensitivity(freqs, **sensitivity_kwargs)
+    noise_to_add = psd ** (1 / 2) * (
+        np.random.normal(0, norm, len(freqs))
+        + 1j * np.random.normal(0, norm, len(freqs))
+    )
+
+    return noise_to_add
