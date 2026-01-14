d2a44ad927330ac64484a73d98dfad5718f6fb50
mikekatz04 <mikekatz04@gmail.com>
2026-01-14 00:36:13 -0800

    new setup brought up to debugging point

--- src/lisatools/cutils/Detector.cu
+++ src/lisatools/cutils/Detector.cu
@@ -7,36 +7,6 @@
 #include <sstream>
 
 
-#if defined(__CUDACC__) || defined(__CUDA_COMPILATION__)
-#define Orbits OrbitsGPU
-#else
-#define Orbits OrbitsCPU
-#endif
-// TODO WHEN BACK FROM BREAK:
-// SEPARATE OUT ANY FUNCTION THAT INCLUDES ANYTHING PYBIND RELATED INTO BINDING. INHERIT THE ORBITS CLASS INTO A WRAPPER CLASS THAT ADDS THE FUNCTIONS THAT SPECIFICALLY TAKE IN NUMPY AND CUPY ARRAYS 
-
-CUDA_DEVICE
-void AddOrbits::add_orbit_information(double dt_, int N_, double *n_arr_, double *L_arr_, double *x_arr_, int *links_, int *sc_r_, int *sc_e_, double armlength_)
-{    
-    printf("new orbits 45454454\n");
-    if (orbits != NULL)
-    {
-        throw std::invalid_argument("Need to allocate orbits before passing into to AddOrbits class method.");
-    }
-    printf("new orbits2\n");
-    orbits = new Orbits(dt_, N_, n_arr_, L_arr_, x_arr_, links_, sc_r_, sc_e_, armlength_);
-    printf("new orbits3\n");
-    printf("orbits: %e\n", orbits->armlength);
-};
-
-void AddOrbits::dealloc()
-{
-    printf("dealloc orbits\n");
-    if (orbits != NULL) 
-        delete orbits;
-}
-
-
 CUDA_DEVICE
 int Orbits::get_window(double t)
 {
